202d0109b2bbf013b7406e0b2a66d0be721c9600
antonio <a.cecere.esterno@almaviva.it>
2020-08-06 17:16:04 +0200

    first commit

---
+++ src/main/java/com/antocecere77/learnreactivespring/itemclient/controller/ItemClientController.java
@@ -0,0 +1,7 @@
+package com.antocecere77.learnreactivespring.itemclient.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ItemClientController {
+}
---
+++ src/test/java/com/antocecere77/learnreactivespring/itemclient/ItemClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antocecere77.learnreactivespring.itemclient;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
